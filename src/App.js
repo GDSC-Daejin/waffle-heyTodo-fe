891ce13d64abd6e8ae6d69b4656cc6d8943dbb98
jieunsse <jieunsse@gmail.com>
2023-03-08 17:42:00 +0900

    1st upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  return (  
+    <>
+      <h3 className="Logo"> 야! 투두! </h3>
+        <input className="Input"></input>
+        <button className="addBtn">추가</button>    
+    </>
   );
 }
 
